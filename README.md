55a49621e15284d530df638cbdff5e72746b585d
Philip Kahn <tigerhawkvok@gmail.com>
2013-10-13 14:53:34 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,28 @@
+D&D Puzzle for The Southern Misfits
+===================================
+
+## Come up with a new name, guys.
+
+## Context:
+
+You find a sheet of paper on an enemy you've yet to encounter. It says the following:
+
+
+## Using this
+
+This program is written to make it easy for you to test your hunches and play with your ciphers. First, install Python:
+
+### Windows
+
+- [Python 3, x86](http://python.org/ftp/python/3.3.2/python-3.3.2.msi)
+- [Python 3, x64](http://python.org/ftp/python/3.3.2/python-3.3.2.amd64.msi)
+
+### OSX
+
+- [Python 3, Intel](http://python.org/ftp/python/3.3.2/python-3.3.2-macosx10.6.dmg)
+- [Python 3, PowerPC](http://python.org/ftp/python/3.3.2/python-3.3.2-macosx10.5.dmg)
+
+### Linux
+
+- [Python 3, Linux](http://python.org/ftp/python/3.3.2/Python-3.3.2.tar.xz)
+- On Ubuntu / Debian, you can run `sudo apt-get install python3` in Terminal.
---
+++ crypter.py
@@ -0,0 +1,34 @@
+import math,re
+
+class Message:
+    def __init__(self,val):
+        # Initialization of class
+        if val.isdigit() is False: 
+            self.cipher=None
+            if re.match("^[A-Za-z .?]*$", val):
+                # Just valid characters
+                self.message=val
+            else:
+                # Bad input
+                print("Invalid message or ciphertext")
+                self.message=None
+        else:
+            # Just digits
+            self.message=None
+            # Check padding/encoding
+            if len(val)%3 is 0:
+                self.cipher=val
+            else: 
+                # Bad input
+                print("Invalid message or ciphertext.")
+                self.cipher=None
+                
+    def encode(self,key=None,*args):
+        if key is None:
+            print("ERROR: No encryption key provided.")
+            return False
+        else:
+            try:
+                print("Continuing")
+            except:
+                print("Unexpected error:", sys.exc_info()[0])
