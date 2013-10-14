76c6c64382cbe4c3f5c36e5fb2d8778754d5c36f
Philip Kahn <tigerhawkvok@gmail.com>
2013-10-13 22:57:37 -0700

    More robust Windows error handling for batch

--- runme-windows.bat
+++ runme-windows.bat
@@ -1,4 +1,11 @@
 @echo off
 rem Batch script!
+python -i main.py
+IF ERRORLEVEL 1 (
+IF EXIST C:\Python33\python.exe (
 C:\Python33\python.exe -i main.py
+) ELSE (
+echo "Could not find Python. Please manually edit this file to point to the correct Python executable, or add python.exe to your PATH."
+)
+)
 pause
