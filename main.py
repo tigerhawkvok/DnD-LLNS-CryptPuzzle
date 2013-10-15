61dc7b073b7076d8f00d4b14f5298483588ca34f
Philip Kahn <tigerhawkvok@gmail.com>
2013-10-15 16:36:33 -0700

    Generalize startup

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 # Simple wrapper to let them interact at the command prompt
 from crypter import Message
 
-print("For use instructions, please see README.md in the same folder.")
-try: string=raw_input("Please input the message or ciphertext you'd like to start with: ") 
-except NameError: string=input("Please input the message or ciphertext you'd like to start with: ")
+print("For use instructions and available commands, please see README.md in the same folder.")
+try: string=raw_input("Please input the message or ciphertext you'd like to start with for message 'm': ") 
+except NameError: string=input("Please input the message or ciphertext you'd like to start with for message 'm': ")
 
 m=Message(string)
-print("You can now use the command as referenced in README.md to test the message 'm'.")
 
