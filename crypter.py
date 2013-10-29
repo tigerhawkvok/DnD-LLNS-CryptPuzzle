8111de98cd10fff4866143281412468be548a2e9
Philip Kahn <tigerhawkvok@gmail.com>
2013-10-28 23:59:45 -0700

    Stabilize new encoding

--- crypter.py
+++ crypter.py
@@ -48,7 +48,8 @@ class Message:
         '9':'41'
         }
     maplen=len(mapping)
-    regex_pattern="^[A-Za-z .?]*$" # regular expression for valid character set
+    regex_pattern="^[A-Za-z0-9 .?\-,]*$" # regular expression for valid character set
+    regex_hex="^[A-Fa-f0-9]*$"
 
     def __init__(self,val=None):
         # Initialization of class
@@ -59,7 +60,7 @@ class Message:
                 self.message=None
             else:
                 # Message is intialized with any value
-                if val.isdigit() is False: 
+                if re.match(self.regex_hex,val) is False: 
                     self.cipher=None
                     if re.match(self.regex_pattern, val):
                         # Just valid characters
@@ -72,7 +73,7 @@ class Message:
                     # Just digits
                     self.message=None
                     # Check padding/encoding
-                    if len(val)%3 is 0:
+                    if len(val)%3 is 0 or len(val)%5 is 0:
                         self.cipher=val
                     else: 
                         # Bad input
@@ -89,7 +90,7 @@ class Message:
                 raise Exception("No encryption key provided.")
             elif key is None and self.key is not None:
                 key=self.key
-            if not re.match("^[A-Za-z .?]*$", key) and key is not None:
+            if not re.match(self.regex_pattern, key) and key is not None:
                 raise Exception("Bad encryption key")
             else:
                 if key is not None:
@@ -107,13 +108,7 @@ class Message:
                     mc=list(rm) # self.message)
                     q=''
                     for c in mc:
-                        cmap=int(self.mapping[c]) # Consider offseting by either message length or key length
-                        num=str(int(math.floor(cmap*factor)))
-                        if len(num) is not 3:
-                            if len(num)==1:
-                                num='00'+num
-                            else:
-                                num='0'+num
+                        num=self.mutateLetter(c,factor)
                         q+=num
                     self.cipher=q
                     return q
@@ -236,24 +231,68 @@ class Message:
             chars=list(m)
             rotated=''
             toSym=dict([reversed(i) for i in self.mapping.items()])
-            self.mapping.update(toSym)
+            #self.mapping.update(toSym)
             for letter in chars:
                 new_num=(int(self.mapping[letter])+n)%self.maplen
                 if new_num is 0: new_num=self.maplen
-                new_letter=self.mapping[str(new_num)]
+                new_letter=toSym[str(new_num)]
                 rotated+=new_letter
             return rotated
         except Exception as inst:
-            print("UNEXPECTED ERROR:",inst)
+            print("UNEXPECTED ROTATION ERROR:",inst)
             return None
     
-    def mutateLetter(self,letter,factor=None):
+    def mutateLetter(self,letter,factor,forward=True):
         # Take a letter, and mutate it to the 4-character version
         try:
-            # Take a letter, get its map, multiply by factor, divide by prime, pad with significand in hex
-            return None
+            if forward:
+                # Take a letter, get its map, multiply by factor, divide by prime, pad with significand in hex
+                # find the biggest prime less than the factor
+                letter=letter.upper()
+                if str(factor).isdigit() is False: 
+                    raise Exception("Bad factor")
+                # first candidates
+                c1=list()
+                ref=list()
+                r=range(2,factor)
+                for n in r:
+                    if n%2 is not 0: 
+                        c1.append(n)
+                c1.reverse()
+                ref=c1
+                hprime=factor
+                for n in ref:
+                    isPrime=True
+                    for n2 in c1:
+                        if n2 < n: 
+                            if (n/n2)%1 == 0:
+                                isPrime=False
+                                break
+                    if isPrime:
+                        hprime=n
+                        break
+                numeric_rep=int(self.mapping[letter])
+                composite=numeric_rep*factor
+                import math
+                significand=math.floor(composite/hprime)
+                cipher=composite%hprime
+                hsig=hex(significand).split('x')[1]
+                hcip=hex(cipher).split('x')[1]
+                # print("Prime:",hprime,"letter",letter,"composite",composite,"Sig:",significand,"Ciph:",cipher,hsig,hcip)
+                spad=2-len(hsig)
+                cpad=3-len(hcip)
+                if cpad is 1: hcip='0'+hcip
+                elif cpad is 2: hcip='00'+hcip
+                if spad is 1: hsig='0'+hsig
+                mutated=hsig+hcip
+                # print(hsig,hcip)
+                # print(mutated.upper())
+                return mutated.upper()
+            else:
+                # undo it!
+                return None
         except Exception as inst:
-            print("UNEXPECTED ERROR:",inst)
+            print("UNEXPECTED MUTATION ERROR:",inst)
             return None
 
     def setKey(self,key):
@@ -290,13 +329,20 @@ class Message:
         # Manually set the cipher
         try:
             if cipher is not None:
-                if cipher.isdigit():
+                #check if it's a hex
+                import re
+                if re.match(self.regex_hex, cipher):
+                    if len(cipher)%5 is 0:
+                        self.cipher=cipher.upper()
+                    else:
+                        raise Exception("Invalid ciphertext length")
+                elif cipher.isdigit():
                     if len(cipher)%3 is 0:
                         self.cipher=cipher.upper()
                     else:
                         raise Exception("Invalid ciphertext length.")
                 else:
-                    raise Exception("Invalid ciphertext. Ciphertext can only have numbers.")
+                    raise Exception("Invalid ciphertext. Ciphertext can only have hex numbers (0-9, A-F).")
             else:
                 raise Exception("Cipher is empty")
         except Exception as inst:
@@ -310,3 +356,204 @@ class Message:
         self.setMessage(*args)
     def setkey(self,*args):
         self.setKey(*args)
+
+    ##########
+    # Old Versions
+    ##########
+    mappingV1 = {
+        'A':'1',
+        'B':'2',
+        'C':'3',
+        'D':'4',
+        'E':'5',
+        'F':'6',
+        'G':'7',
+        'H':'8',
+        'I':'9',
+        'J':'10',
+        'K':'11',
+        'L':'12',
+        'M':'13',
+        'N':'14',
+        'O':'15',
+        'P':'16',
+        'Q':'17',
+        'R':'18',
+        'S':'19',
+        'T':'20',
+        'U':'21',
+        'V':'22',
+        'W':'23',
+        'X':'24',
+        'Y':'25',
+        'Z':'26',
+        ' ':'27',
+        '.':'28',
+        '?':'29'
+        }
+    maplenV1=len(mappingV1)
+
+    def getFactorV1(self):
+        # Work with the key
+        try:
+            if self.key is None:
+                raise Exception("No encryption key has been set. Run encode(), decode(), or setKey() with an encryption key first")
+            import math
+            chars=list(self.key)
+            nums=list()
+            n=range(len(chars)) # The iterator
+            for letter in chars:
+                nums.append(self.mappingV1[letter])
+            # Do the math
+            numerator=0 # summation holder
+            k=0 # second summation holder
+            for i in n:
+                l=i+1
+                numerator+=int(nums[i])*l
+                k+=int(nums[i])
+            exp=math.floor(math.log(k*len(chars),10)) 
+            denominator=math.pow(10,exp)
+            factor=numerator/denominator
+            return factor
+        except Exception as inst:
+            print("ERROR:",inst)
+            return None
+
+
+    def decodeV1(self,key=None,cipher=None):
+        import re
+        try:        
+            if key is None and self.key is None:
+                raise Exception("No decryption key provided.")
+            elif key is None and self.key is not None:
+                key=self.key
+            if not re.match(self.regex_pattern, key) and key is not None:
+                raise Exception("Bad decryption key")
+            else:
+                if key is not None:
+                    self.key=key.upper()
+                    if cipher is not None and cipher.isdigit():
+                        if len(cipher)%3 is 0:
+                            self.cipher=cipher.upper()
+                        else:
+                            print("Invalid ciphertext length. Attempting to use stored value")
+                    elif cipher is not None and not cipher.isdigit():
+                        print("Invalid ciphertext. Attempting to use stored value")
+                    if self.cipher is not None:
+                        # Decrypt
+                        factor=self.getFactorV1()
+                        # Bucket the characters in 3
+                        cl=list(self.cipher)
+                        cb=list()
+                        i=1
+                        t=''
+                        for l in cl:
+                            t+=str(l)
+                            if i%3 is 0:
+                                cb.append(t)
+                                t=''
+                            i+=1
+                        q=''
+                        toSym=dict([reversed(i) for i in self.mappingV1.items()])
+                        self.mappingV1.update(toSym)
+                        for c in cb:
+                            try:
+                                letter=self.mappingV1[str(int(math.ceil(int(c)/factor)))]
+                            except KeyError:
+                                # Replace a 'bad' map from a bad key with a blank
+                                letter=''
+                            q+=letter
+                        uq=self.rotV1(q,'-'+str(len(self.key)))
+                        print(uq)
+                        self.message=uq
+                    else:
+                        raise Exception("No valid ciphertext to decrypt")
+        except Exception as inst:
+            print("ERROR:", inst)
+            return None
+
+    def rotV1(self,m=None,n=None):
+        # Rotate a message 'm' by 'n'
+        try:
+            if n is None or str(abs(int(n))).isdigit() is False:
+                if m is not None:
+                    try:
+                        if str(abs(int(m))).isdigit() is True:
+                            n=m
+                    except TypeError:
+                        # abs or something didn't work
+                        pass
+                else:
+                    try:
+                        n=len(self.key)
+                    except TypeError:
+                        # the key hasn't been defined
+                        raise Exception("No key has been defined yet, and no rotation size has been provided.")
+            else: n=int(n) # just in case
+        except Exception as inst:
+            print("ERROR: Invalid Rotation Length - ",inst);
+            return None
+        try:
+            if m is not None:
+                import re
+                if re.match(self.regex_pattern, m):
+                    # Just valid characters
+                    m=m.upper()
+                else:
+                    raise Exception("Invalid message text")
+            else:
+                m=self.message
+            chars=list(m)
+            rotated=''
+            toSym=dict([reversed(i) for i in self.mappingV1.items()])
+            self.mappingV1.update(toSym)
+            for letter in chars:
+                new_num=(int(self.mappingV1[letter])+n)%self.maplenV1
+                if new_num is 0: new_num=self.maplenV1
+                new_letter=self.mappingV1[str(new_num)]
+                rotated+=new_letter
+            return rotated
+        except Exception as inst:
+            print("UNEXPECTED ERROR:",inst)
+            return None
+
+    def encodeV1(self,key=None,message=None):
+        import re
+        try:
+            if key is None and self.key is None:
+                raise Exception("No encryption key provided.")
+            elif key is None and self.key is not None:
+                key=self.key
+            if not re.match("^[A-Za-z .?]*$", key) and key is not None:
+                raise Exception("Bad encryption key")
+            else:
+                if key is not None:
+                    self.key=key.upper()
+                if message is not None:
+                    if re.match(self.regex_pattern, message):
+                        # Just valid characters
+                        self.message=message.upper()
+                    else:
+                        # Bad input
+                        print("Invalid message or ciphertext, attempting to use stored value")
+                if self.message is not None:
+                    factor=self.getFactorV1()
+                    rm=self.rotV1()
+                    mc=list(rm) # self.message)
+                    q=''
+                    for c in mc:
+                        cmap=int(self.mappingV1[c]) # Consider offseting by either message length or key length
+                        num=str(int(math.floor(cmap*factor)))
+                        if len(num) is not 3:
+                            if len(num)==1:
+                                num='00'+num
+                            else:
+                                num='0'+num
+                        q+=num
+                    self.cipher=q
+                    return q
+                else:
+                    raise Exception("No valid message to encrypt")
+        except Exception as inst:
+            print("ERROR:", inst)
+            return None
