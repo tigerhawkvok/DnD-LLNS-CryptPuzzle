ef7b4784d9e227d4bd19bc3aa56bc51035cd3b12
Philip Kahn <tigerhawkvok@gmail.com>
2013-11-02 01:18:58 -0700

    Shell script messages

--- runme-linux.sh
+++ runme-linux.sh
@@ -1,14 +1,17 @@
 # Shell script
 if hash python3 2>/dev/null
 then 
-python3 -i main.py
+    python3 -i main.py
 else
-echo `Attempting to install python 3`
-sudo apt-get install python3
-if hash python3 2>/dev/null
-then 
-python3 -i main.py
-else
-python -i main.py
-fi
+    echo `Attempting to install python 3`
+    sudo apt-get install python3
+    if hash python3 2>/dev/null
+    then 
+        python3 -i main.py
+    else
+        echo `You may experience problems running this in an older version of Python.`
+        echo `I strongly suggest upgrading to Python 3.`
+        read -p `Press [Enter] to continue ...`
+        python -i main.py
+    fi
 fi
