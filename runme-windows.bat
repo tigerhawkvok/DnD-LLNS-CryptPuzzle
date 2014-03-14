b187846a5a271dcaa1901f24c402285d0ea28e36
Philip Kahn <tigerhawkvok@gmail.com>
2014-03-13 21:55:33 -0700

    Remove prompt to terminate batch

--- runme-windows.bat
+++ runme-windows.bat
@@ -1,11 +1,10 @@
 @echo off
 rem Batch script!
-python -i main.py
+start python -i main.py
 IF ERRORLEVEL 1 (
 IF EXIST C:\Python33\python.exe (
-C:\Python33\python.exe -i main.py
+start C:\Python33\python.exe -i main.py
 ) ELSE (
 echo "Could not find Python. Please manually edit this file to point to the correct Python executable, or add python.exe to your PATH."
 )
 )
-pause
